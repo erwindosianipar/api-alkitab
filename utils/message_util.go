bea3dadff4c46f1be82ac3b45a43c9d0e2b3e04d
Erwindo Sianipar <erwindo.sianipar@thelionparcel.com>
2020-11-16 20:51:31 +0700

    chore: add file utils component

---
+++ utils/message_util.go
@@ -0,0 +1,11 @@
+package utils
+
+const (
+	ErrorSomethingWentWrong = "Error something went wrong"
+	ErrorCallAPI            = "Error to call API"
+	ErrorReadResponseData   = "Error read response data"
+	ErrorDecodeResponse     = "Error decode response"
+	ChapterMustValid        = "Chapter only accept a valid number, docs https://api-alkitab.herokuapp.com"
+	VerseMustValid          = "Verse only accept a valid number, docs https://api-alkitab.herokuapp.com"
+	ErrorVersionType        = "Version only accept tb/bis/net value, docs https://api-alkitab.herokuapp.com"
+)

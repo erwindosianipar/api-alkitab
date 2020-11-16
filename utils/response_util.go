985ae273952ef17bbd4abd95c2de74f8dda09152
Erwindo Sianipar <erwindo.sianipar@thelionparcel.com>
2020-11-16 20:55:47 +0700

    chore: add response utilities

--- utils/response_util.go
+++ utils/response_util.go
@@ -12,7 +12,14 @@ func Message(status int, message string) map[string]interface{} {
 	}
 }
 
-func Response(w http.ResponseWriter, code int, data map[string]interface{}) {
+func ResponseMessage(w http.ResponseWriter, code int, data map[string]interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+func ResponseData(w http.ResponseWriter, code int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 

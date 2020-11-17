acf56c0ec2bd845463edaa611be85bf06031c7ba
Erwindo Sianipar <erwindo.sianipar@thelionparcel.com>
2020-11-17 17:23:10 +0700

    test: add passage test file

---
+++ passage/handler/passage_test.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"testing"
+)
+
+func TestPassageChapter(t *testing.T) {
+	t.Run("test passage chapter", func(t *testing.T) {
+
+	})
+}
+
+func TestPassageChapterVerse(t *testing.T) {
+	t.Run("test passage chapter verse", func(t *testing.T) {
+
+	})
+}
+
+func TestPassageChapterV2(t *testing.T) {
+	t.Run("test passage chapter v2", func(t *testing.T) {
+
+	})
+}
+
+func TestPassageChapterVerseV2(t *testing.T) {
+	t.Run("test passage chapter verse v2", func(t *testing.T) {
+
+	})
+}
